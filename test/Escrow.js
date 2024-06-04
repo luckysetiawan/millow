576b462d9d2ea5ce86bc80e7b04bc3c67df59d22
luckysetiawan <luckysetiawan0802@gmail.com>
2024-06-04 17:37:35 +0700

    update test escrow

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,10 +6,33 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
-    it('saves the addresses', async () => {
-        const RealEstateFactory = await ethers.getContractFactory('RealEstate');
-        const realEstate = await RealEstateFactory.deploy();
+    let buyer, seller, inspector, lender;
+    let realEstate, escrow;
 
-        console.log(await realEstate.getAddress());
+    it('saves the addresses', async () => {
+        // Setup accounts
+        [ buyer, seller, inspector, lender ] = await ethers.getSigners();
+
+        // Deploy Real Estate
+        const RealEstateFactory = await ethers.getContractFactory('RealEstate');
+        realEstate = await RealEstateFactory.deploy();
+        // console.log(await realEstate.getAddress());
+
+        // Mint
+        let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
+        await transaction.wait()
+
+        const EscrowFactory = await ethers.getContractFactory('Escrow');
+        escrow = await EscrowFactory.deploy(
+            realEstate.getAddress(),
+            seller.getAddress(),
+            inspector.getAddress(),
+            lender.getAddress()
+        );
+
+        expect(await escrow.nftAddress()).to.be.equal(await realEstate.getAddress());
+        expect(await escrow.seller()).to.be.equal(await seller.getAddress());
+        expect(await escrow.inspector()).to.be.equal(await inspector.getAddress());
+        expect(await escrow.lender()).to.be.equal(await lender.getAddress());
     })
 })
