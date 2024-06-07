4b3224554f58f275f01b79f4c31e6f1320717af1
luckysetiawan <luckysetiawan0802@gmail.com>
2024-06-07 13:57:40 +0700

    add approve sale with test

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -37,6 +37,7 @@ contract Escrow {
     mapping(uint256 => uint256) public escrowAmount;
     mapping(uint256 => address) public buyer;
     mapping(uint256 => bool) public inspectionPassed;
+    mapping(uint256 => mapping(address => bool)) public approval;
 
     constructor(
         address _nftAddress,
@@ -76,6 +77,11 @@ contract Escrow {
         bool _passed
     ) public onlyInspector {
         inspectionPassed[_nftID] = _passed;
+    }
+
+    // Approve Sale
+    function approveSale(uint256 _nftID) public {
+        approval[_nftID][msg.sender] = true;
     }
 
     receive() external payable {}
--- test/Escrow.js
+++ test/Escrow.js
@@ -94,5 +94,23 @@ describe('Escrow', () => {
             expect(await escrow.inspectionPassed(1)).to.be.equal(true);
         })
     })
+
+    describe('Approval', () => {
+        it('Updates approval status', async () => {
+            let transaction = await escrow.connect(buyer).approveSale(1);
+            await transaction.wait();
+
+            transaction = await escrow.connect(seller).approveSale(1);
+            await transaction.wait();
+
+            transaction = await escrow.connect(lender).approveSale(1);
+            await transaction.wait();
+
+
+            expect(await escrow.approval(1, buyer.getAddress())).to.be.equal(true);
+            expect(await escrow.approval(1, seller.getAddress())).to.be.equal(true);
+            expect(await escrow.approval(1, buyer.getAddress())).to.be.equal(true);
+        })
+    })
     
 })
