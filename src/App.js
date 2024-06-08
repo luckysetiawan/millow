171f47ec574aab87dcf3bc29ad5b02be6567f9e3
luckysetiawan <luckysetiawan0802@gmail.com>
2024-06-08 12:48:15 +0700

    integrate homes data

--- src/App.js
+++ src/App.js
@@ -15,16 +15,38 @@ import config from './config.json';
 
 function App() {
 
+  const [provider, setProvider] = useState(null);
+  const [escrow, setEscrow] = useState(null);
+
   const [account, setAccount] = useState(null);
+  const [homes, setHomes] = useState([]);
 
   const loadBlockchainData = async () => {
+    console.log('MASUK');
     const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
+
+    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider);
+    const totalSupply = await realEstate.totalSupply();
+    const homes = [];
+
+    for (let i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      homes.push(metadata);
+    }
+    setHomes(homes);
+
+    const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider);
+    setEscrow(escrow);
 
     window.ethereum.on('accountsChanged', async () => {
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
       const account = ethers.getAddress(accounts[0])
       setAccount(account);
-    })
+    });
   }
 
   useEffect(() => {
@@ -33,30 +55,31 @@ function App() {
 
   return (
     <div>
-
       <Navigation account={account} setAccount={setAccount} />
       <Search />
 
       <div className='cards__section'>
 
-        <h3>Homes for you</h3>
+        <h3>Homes For You</h3>
         <hr />
 
         <div className='cards'>
-          <div className='card'>
-            <div className='card__image'>
-              <img alt="Home" />
+          {homes.map((home, index) => (
+            <div className='card' key={index}>
+              <div className='card__image'>
+                <img src={home.image} alt="Home" />
+              </div>
+              <div className='card__info'>
+                <h4>{home.attributes[0].value} ETH</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong> beds |
+                  <strong>{home.attributes[3].value}</strong> bath |
+                  <strong>{home.attributes[4].value}</strong> m<sup>2</sup>
+                </p>
+                <p>{home.address}</p>
+              </div>
             </div>
-            <div className='card__info'>
-              <h4>1 ETH</h4>
-              <p>
-                <strong>1</strong> beds |
-                <strong>2</strong> bath |
-                <strong>3</strong> m<sup>2</sup>
-              </p>
-              <p>123 StreetName St</p>
-            </div>
-          </div>
+          ))}
         </div>
 
       </div>
