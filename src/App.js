c00bad4c5e2da1d09b9a020b5be16d23dae69174
luckysetiawan <luckysetiawan0802@gmail.com>
2024-06-08 12:09:31 +0700

    implement front end meta mask connection

--- src/App.js
+++ src/App.js
@@ -15,11 +15,28 @@ import config from './config.json';
 
 function App() {
 
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.getAddress(accounts[0])
+      setAccount(account);
+    })
+  }
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
   return (
     <div>
 
       <div className='cards__section'>
 
+        <Navigation account={account} setAccount={setAccount} />
         <h3>Welcome to Millow</h3>
 
       </div>
