995a183aac8c9f2ecbe759d9e79affa8d1950e5e
luckysetiawan <luckysetiawan0802@gmail.com>
2024-06-08 15:18:39 +0700

    update text

--- src/components/Home.js
+++ src/components/Home.js
@@ -112,9 +112,9 @@ const Home = ({ home, provider, account, escrow, togglePop }) => {
                 <div className="home__overview">
                     <h1>{home.name}</h1>
                     <p>
-                        <strong>{home.attributes[2].value}</strong> bds |
-                        <strong>{home.attributes[3].value}</strong> ba |
-                        <strong>{home.attributes[4].value}</strong> sqft
+                        <strong>{home.attributes[2].value}</strong> beds |
+                        <strong>{home.attributes[3].value}</strong> bath |
+                        <strong>{home.attributes[4].value}</strong> m<sup>2</sup>
                     </p>
                     <p>{home.address}</p>
 
